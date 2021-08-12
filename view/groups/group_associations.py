7b22c37e2cf543564b255df5a41be16269610409
Christopher Horn <cdhorn@embarqmail.com>
2021-08-11 21:53:34 -0400

    feat: association list dnd support

--- view/groups/group_associations.py
+++ view/groups/group_associations.py
@@ -32,11 +32,26 @@ from gi.repository import Gtk
 
 # ------------------------------------------------------------------------
 #
+# Gramps modules
+#
+# ------------------------------------------------------------------------
+from gramps.gen.const import GRAMPS_LOCALE as glocale
+from gramps.gen.db import DbTxn
+from gramps.gen.display.name import displayer as name_displayer
+from gramps.gen.lib import PersonRef
+from gramps.gui.editors import EditPersonRef
+from gramps.gen.errors import WindowActiveError
+
+
+# ------------------------------------------------------------------------
+#
 # Plugin modules
 #
 # ------------------------------------------------------------------------
 from ..frames.frame_association import AssociationGrampsFrame
 from .group_list import GrampsFrameGroupList
+
+_ = glocale.translation.sgettext
 
 
 # ------------------------------------------------------------------------
@@ -51,7 +66,7 @@ class AssociationsGrampsFrameGroup(GrampsFrameGroupList):
     """
 
     def __init__(self, grstate, groptions, obj):
-        GrampsFrameGroupList.__init__(self, grstate, groptions, enable_drop=False)
+        GrampsFrameGroupList.__init__(self, grstate, groptions)
         self.obj = obj
         self.obj_type = "Person"
         if not self.get_layout("tabbed"):
@@ -66,3 +81,71 @@ class AssociationsGrampsFrameGroup(GrampsFrameGroupList):
             )
             self.add_frame(frame)
         self.show_all()
+
+    def save_reordered_list(self):
+        """
+        Save a reordered list of associations.
+        """
+        new_list = []
+        for frame in self.row_frames:
+            for ref in self.obj.get_person_ref_list():
+                if ref.ref == frame.primary.obj.get_handle():
+                    new_list.append(ref)
+                    break
+        action = "{} {} {} {} {}".format(
+            _("Reordered"),
+            _("Associations"),
+            _("for"),
+            _("Person"),
+            self.obj.get_gramps_id(),
+        )
+        with DbTxn(action, self.grstate.dbstate.db) as trans:
+            self.obj.set_person_ref_list(new_list)
+            self.grstate.dbstate.db.commit_person(self.obj, trans)
+
+    def save_new_object(self, handle, insert_row):
+        """
+        Add a new person to the list of associations.
+        """
+        for frame in self.row_frames:
+            if frame.primary.obj.get_handle() == handle:
+                return
+
+        person_ref = PersonRef()
+        person_ref.ref = handle
+        callback = lambda x: self.save_new_person(x, insert_row)
+        try:
+            EditPersonRef(
+                self.grstate.dbstate,
+                self.grstate.uistate,
+                [],
+                person_ref,
+                callback
+            )
+        except WindowActiveError:
+            pass
+
+    def save_new_person(self, person_ref, insert_row):
+        """
+        Save the new person added to the list of associations.
+        """
+        new_list = []
+        for frame in self.row_frames:        
+            for ref in self.obj.get_person_ref_list():
+                if ref.ref == frame.primary.obj.get_handle():
+                    new_list.append(ref)
+        new_list.insert(insert_row, person_ref)
+        person = self.grstate.dbstate.db.get_person_from_handle(person_ref.ref)
+        action = "{} {} {} {} {} {} {}".format(
+            _("Added"),
+            _("Person"),
+            person.get_gramps_id(),
+            _("Association"),
+            _("to"),
+            _("Person"),
+            self.obj.get_gramps_id(),
+        )
+        with DbTxn(action, self.grstate.dbstate.db) as trans:
+            self.obj.set_person_ref_list(new_list)
+            self.grstate.dbstate.db.commit_person(self.obj, trans)
+
